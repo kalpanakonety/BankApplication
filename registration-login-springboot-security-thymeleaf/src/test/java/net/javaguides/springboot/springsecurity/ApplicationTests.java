125cb91af672d81ac29df0a46b3a098ab3e4df48
Kalpana Konety <Kalpana Konety@192.168.0.151>
2020-07-28 18:04:18 +0100

    second commit

--- registration-login-springboot-security-thymeleaf/src/test/java/net/javaguides/springboot/springsecurity/ApplicationTests.java
+++ registration-login-springboot-security-thymeleaf/src/test/java/net/javaguides/springboot/springsecurity/ApplicationTests.java
@@ -1,19 +1,10 @@
 package net.javaguides.springboot.springsecurity;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import net.javaguides.springboot.springsecurity.repository.UserRepository;
-import net.javaguides.springboot.springsecurity.service.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,22 +14,5 @@ public class ApplicationTests {
 	public void contextLoads() {
 	}
 
-	@Autowired
-	private UserService service;
-	
-	@MockBean
-	private UserRepository userrepo;
-	
-	@Test
-	public void findbyEmailTest() {
-		
-		
-	}
-	
-	public void saveTest() {
-	//	when(userrepo.findAll()).thenReturn(Stream.of(arg0))
-
-	//	assertEquals();
-	}
 	
 }
